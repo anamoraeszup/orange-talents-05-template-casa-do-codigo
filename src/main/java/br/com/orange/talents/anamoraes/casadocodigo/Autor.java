739b6627c916751d479cca8ec3b41f24acf60b57
Ana Moraes <ana.moraes@zup.com.br>
2021-05-17 17:50:17 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/orange/talents/anamoraes/casadocodigo/Autor.java
@@ -0,0 +1,13 @@
+package br.com.orange.talents.anamoraes.casadocodigo;
+
+import java.util.Date;
+
+public class Autor {
+
+	private String nomeAutor;
+	private String emailAutor;
+	private String descricaoAutor;
+	private Date dataCriacaoAutor;
+	
+	
+}
---
+++ src/test/java/br/com/orange/talents/anamoraes/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orange.talents.anamoraes.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
